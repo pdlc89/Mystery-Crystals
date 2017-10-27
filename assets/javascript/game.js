5a2f34ec526d97cf4ad8689a79c355709593f6b1
Pedro <pcruz9332@gmail.com>
2017-10-26 21:27:09 -0700

    thursday-progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,8 +14,6 @@ var isStartChosen;
 	 computerGuess= "";
 	 usersChoice = "";
 	 userGuess = "";
-	 gamesWon = 0;
-	 gamesLost = 0;
 	 chosenCrystal = false;
 
 	 $("#computer-guess, #user-tally").empty();
@@ -23,19 +21,29 @@ var isStartChosen;
   	}
 
   function GameInProgress() {
+	 console.log("IN Progress " + userGuess);
+  	}
+  function gWins() {
 	 computerGuess= "";
 	 userGuess = "";
-	 holder = "";
+	 usersChoice = "";
 	 isStartChosen = false;
-
 	 $("#computer-guess, #user-tally").empty();
-	 alert("Please Press: \"Start\"");
+	 alert("Please press: \"Start\"");
   	}
+
+  	function gameLost() {
+  		computerGuess = "";
+  		userGuess = "";
+  		usersChoice ="";
+  		isStartChosen = false;
+  		$("#computerGuess, #user-tally").empty();
+  		alert("Please press: \"Start\"");
+  	};
 
 	$("#start-button").on("click", function(){
 		var numbersArray = [23, 13, 54, 67, 8944, 4, 7, 12];
 		computerGuess = numbersArray[Math.floor(Math.random()*numbersArray.length)];
-		console.log(computerGuess);
 		$("#computer-guess").text(computerGuess);
 		isStartChosen = true;
 		userGuess = 0;
@@ -46,26 +54,28 @@ var isStartChosen;
   		if (isStartChosen) {  
 			usersChoice += this.value;
 			isCrystalChosen = true
-
+			$("#user-tally").empty();	
 	  		if (isCrystalChosen){
 				usersChoice = parseInt(usersChoice);
 				userGuess = parseInt(userGuess);
-				userGuess = userGuess + usersChoice;
+				userGuess += usersChoice;
 				$("#user-tally").text(userGuess);
+				GameInProgress();
 			}
 			if (userGuess <computerGuess) return;
-
   			if (userGuess === computerGuess){
 	  			gamesWon++;
 				$("#user-tally").text("You've Won!!!");
 				$("#wins").text(gamesWon);
-				setTimeout(GameInProgress, 1000);
+				setTimeout(gWins, 1000);
+				console.log("wins");
   			}
   			if (userGuess > computerGuess) {
+  				console.log("Lost");
 	  			gamesLost++;
 	  			$("#user-tally").text("You got to greedy!");
 	  			$("#loses").text(gamesLost);
-	  			setTimeout(GameStart, 1000);
+	  			setTimeout(gameLost, 1000);
   				}
   		}
   		else {
