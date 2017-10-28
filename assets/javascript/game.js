f2b57c45214ca582da27fbe797b06d9eb9b8f9cf
PEDRO DE LA CRUZ MARTINEZ <pcruz9332@gmail.com>
2017-10-27 19:52:31 -0700

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,37 +2,44 @@ $(document).ready(function() {
 
 //Declares the global variables to be used
 var computerGuess;
-var userGuess = "";
+var userGuess;
 var usersChoice = "";
 var userTally = "";
 var gamesPlayed;
 var gamesWon = "";
 var gamesLost = "";
 var isStartChosen;
-
+var isCrystalChosen;
+var isFirstGameOn = false;
+var ifFirstGameOn = true;
+var holder;
+//clears the form but keeps the tally of wins/loses
   function GameStart() {
 	 computerGuess= "";
 	 usersChoice = "";
 	 userGuess = "";
 	 chosenCrystal = false;
-
 	 $("#computer-guess, #user-tally").empty();
 	 alert("Please Press: \"Start\"");
-  	}
+  	};
 
-  function GameInProgress() {
-	 console.log("IN Progress " + userGuess);
-  	}
   function gWins() {
-	 computerGuess= "";
-	 userGuess = "";
-	 usersChoice = "";
-	 isStartChosen = false;
-	 $("#computer-guess, #user-tally").empty();
-	 alert("Please press: \"Start\"");
-  	}
+  	console.log("win!");
+  	gamesWon++;
+	$("#wins").text(gamesWon);
+	computerGuess= "";
+	userGuess = "";
+	usersChoice = "";
+	isStartChosen = false;
+	$("#computer-guess, #user-tally").empty();
+	alert("Please press: \"Start\"");
+  	};
 
   	function gameLost() {
+
+  		console.log("Lost");
+	  	gamesLost++;
+	  	$("#loses").text(gamesLost);
   		computerGuess = "";
   		userGuess = "";
   		usersChoice ="";
@@ -42,39 +49,46 @@ var isStartChosen;
   	};
 
 	$("#start-button").on("click", function(){
-		var numbersArray = [23, 13, 54, 67, 8944, 4, 7, 12];
+		var numbersArray = [23, 13, 54, 67, 89, 44, 4, 7, 12];
 		computerGuess = numbersArray[Math.floor(Math.random()*numbersArray.length)];
 		$("#computer-guess").text(computerGuess);
 		isStartChosen = true;
 		userGuess = 0;
 	});
-
-  
+//listen for user click
   $(".crystal").on("click", function(){
-  		if (isStartChosen) {  
-			usersChoice += this.value;
-			isCrystalChosen = true
-			$("#user-tally").empty();	
-	  		if (isCrystalChosen){
-				usersChoice = parseInt(usersChoice);
-				userGuess = parseInt(userGuess);
-				userGuess += usersChoice;
-				$("#user-tally").text(userGuess);
-				GameInProgress();
-			}
-			if (userGuess <computerGuess) return;
+  	//check if start is chosen and if it is: store the value given to the image
+  		if (isStartChosen) {
+  			$("#user-tally").empty();  
+			usersChoice = this.value;
+			userGuess = parseInt(userGuess);
+			usersChoice = parseInt(usersChoice);
+			userGuess = userGuess + usersChoice;
+			$("#user-tally").text(userGuess);
+//checks if the user has exceeded count
+			if (userGuess < computerGuess) return;
+//checks to see if the user won
   			if (userGuess === computerGuess){
-	  			gamesWon++;
-				$("#user-tally").text("You've Won!!!");
-				$("#wins").text(gamesWon);
+  				$("#user-tally").text("You've Won!!!");
+  				 $( "#user-tally" ).animate({
+   				 width: "600px",
+   				 opacity: 0.5,
+   				 marginLeft: "0.6in",
+   				 fontSize: "3em",
+   				 borderWidth: "10px"
+  				}, 800, function(){$(this).removeAttr("style")});
 				setTimeout(gWins, 1000);
-				console.log("wins");
   			}
-  			if (userGuess > computerGuess) {
-  				console.log("Lost");
-	  			gamesLost++;
-	  			$("#user-tally").text("You got to greedy!");
-	  			$("#loses").text(gamesLost);
+// checks to see if te user lost
+  			else {
+  				$("#user-tally").text("You got too greedy!");
+  				$( "#user-tally" ).animate({
+   				 width: "600px",
+   				 opacity: 0.5,
+   				 marginLeft: "0.6in",
+   				 fontSize: "3em",
+   				 borderWidth: "10px"
+  				}, 800, function(){$(this).removeAttr("style")});
 	  			setTimeout(gameLost, 1000);
   				}
   		}
