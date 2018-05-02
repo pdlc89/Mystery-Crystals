d1a9c5b06ed63c36e9b1d8330f5833992ddce99e
pdlc89 <pcruz9332@gmail.com>
2018-05-01 20:48:20 -0700

    mobile

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,100 +1,112 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-//Declares the global variables to be used
-var computerGuess;
-var userGuess;
-var usersChoice = "";
-var userTally = "";
-var gamesPlayed;
-var gamesWon = "";
-var gamesLost = "";
-var isStartChosen;
-var isCrystalChosen;
-var isFirstGameOn = false;
-var ifFirstGameOn = true;
-var holder;
-//clears the form but keeps the tally of wins/loses
-  function GameStart() {
-	 computerGuess= "";
-	 usersChoice = "";
-	 userGuess = "";
-	 chosenCrystal = false;
-	 $("#computer-guess, #user-tally").empty();
-	 alert("Please Press: \"Start\"");
-  	};
+	//Declares the global variables to be used
+	var computerGuess;
+	var userGuess;
+	var usersChoice = "";
+	var userTally = "";
+	var gamesPlayed;
+	var gamesWon = "";
+	var gamesLost = "";
+	var isStartChosen;
+	var isCrystalChosen;
+	var isFirstGameOn = false;
+	var ifFirstGameOn = true;
+	var holder;
+	//clears the form but keeps the tally of wins/loses
+	function GameStart() {
+		computerGuess = "";
+		usersChoice = "";
+		userGuess = "";
+		chosenCrystal = false;
+		$("#computer-guess, #user-tally").empty();
+		
+	};
 
-  function gWins() {
-  	console.log("win!");
-  	gamesWon++;
-	$("#wins").text(gamesWon);
-	computerGuess= "";
-	userGuess = "";
-	usersChoice = "";
-	isStartChosen = false;
-	$("#computer-guess, #user-tally").empty();
-	alert("Please press: \"Start\"");
-  	};
+	function gWins() {
+		console.log("win!");
+		gamesWon++;
+		$("#wins").text(gamesWon);
+		computerGuess = "";
+		userGuess = "";
+		usersChoice = "";
+		isStartChosen = false;
+		$("#computer-guess, #user-tally").empty();
+		$("#message").empty()
+		$("#message").prepend('Please Press "Start');
+	};
 
-  	function gameLost() {
+	function gameLost() {
 
-  		console.log("Lost");
-	  	gamesLost++;
-	  	$("#loses").text(gamesLost);
-  		computerGuess = "";
-  		userGuess = "";
-  		usersChoice ="";
-  		isStartChosen = false;
-  		$("#computerGuess, #user-tally").empty();
-  		alert("Please press: \"Start\"");
-  	};
+		console.log("Lost");
+		gamesLost++;
+		$("#loses").text(gamesLost);
+		computerGuess = "";
+		userGuess = "";
+		usersChoice = "";
+		isStartChosen = false;
+		$("#computerGuess, #user-tally").empty();
+		$("#message").empty()
+		$("#message").prepend('Please Press <b>"Start</b>');
+	};
 
-	$("#start-button").on("click", function(){
+	$("#start").on("click", function () {
+		console.log("work")
 		var numbersArray = [23, 13, 54, 67, 89, 44, 4, 7, 12];
-		computerGuess = numbersArray[Math.floor(Math.random()*numbersArray.length)];
+		computerGuess = numbersArray[Math.floor(Math.random() * numbersArray.length)];
 		$("#computer-guess").text(computerGuess);
 		isStartChosen = true;
 		userGuess = 0;
+		$("#start").empty();
+		$("#start").prepend("Good Luck!")
+		$("#message").empty()
 	});
-//listen for user click
-  $(".crystal").on("click", function(){
-  	//check if start is chosen and if it is: store the value given to the image
-  		if (isStartChosen) {
-  			$("#user-tally").empty();  
+	//listen for user click
+	$(".crystal").on("click", function () {
+		//check if start is chosen and if it is: store the value given to the image
+		if (isStartChosen) {
+			$("#user-tally").empty();
 			usersChoice = this.value;
 			userGuess = parseInt(userGuess);
 			usersChoice = parseInt(usersChoice);
 			userGuess = userGuess + usersChoice;
 			$("#user-tally").text(userGuess);
-//checks if the user has exceeded count
+			//checks if the user has exceeded count
 			if (userGuess < computerGuess) return;
-//checks to see if the user won
-  			if (userGuess === computerGuess){
-  				$("#user-tally").text("You've Won!!!");
-  				 $( "#user-tally" ).animate({
-   				 width: "600px",
-   				 opacity: 0.5,
-   				 marginLeft: "0.6in",
-   				 fontSize: "3em",
-   				 borderWidth: "10px"
-  				}, 800, function(){$(this).removeAttr("style")});
+			//checks to see if the user won
+			if (userGuess === computerGuess) {
+				$("#start").empty();
+				$("#start").prepend("Start") 
+				$("#user-tally").text("You've Won!!!");
+				$("#user-tally").animate({
+					width: "600px",
+					opacity: 0.5,
+					marginLeft: "0.6in",
+					fontSize: "3em",
+					borderWidth: "10px"
+				}, 800, function () { $(this).removeAttr("style") });
 				setTimeout(gWins, 1000);
-  			}
-// checks to see if te user lost
-  			else {
-  				$("#user-tally").text("You got too greedy!");
-  				$( "#user-tally" ).animate({
-   				 width: "600px",
-   				 opacity: 0.5,
-   				 marginLeft: "0.6in",
-   				 fontSize: "3em",
-   				 borderWidth: "10px"
-  				}, 800, function(){$(this).removeAttr("style")});
-	  			setTimeout(gameLost, 1000);
-  				}
-  		}
-  		else {
-  			GameStart();
-  		}
-  });
+			}
+			// checks to see if te user lost
+			else {
+				$("#user-tally").text("You got too greedy!");
+				$("#user-tally").animate({
+					width: "600px",
+					opacity: 0.5,
+					marginLeft: "0.6in",
+					fontSize: "3em",
+					borderWidth: "10px"
+				}, 800, function () {
+					$("#start").empty();
+					$("#start").prepend("Start") 
+					$(this).removeAttr("style")
+				});
+				setTimeout(gameLost, 1000);
+			}
+		}
+		else {
+			GameStart();
+		}
+	});
 });
 
